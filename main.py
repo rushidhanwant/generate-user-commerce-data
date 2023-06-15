8e99157cea90e36db4d54d1d726599c83fab9303
rushikesh dhanwant <rushikeshdhanwant@gmail.com>
2023-06-15 20:17:13 +0530

    updated file path

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from product import generate_product_data
 from user import generate_user_data
 
 user_data = generate_user_data()
-pd.read_json(json.dumps(user_data, indent=4)).to_csv('user_data.csv')
+pd.read_json(json.dumps(user_data, indent=4)).to_csv('./generated_data/user_data.csv')
 
 product_data = generate_product_data()
-pd.read_json(json.dumps(product_data, indent=4)).to_csv('product_data.csv')
+pd.read_json(json.dumps(product_data, indent=4)).to_csv('./generated_data/product_data.csv')
 
 orders_data = generate_order_data(user_data, product_data)
-pd.read_json(json.dumps(orders_data, indent=4)).to_csv('orders_data.csv')
+pd.read_json(json.dumps(orders_data, indent=4)).to_csv('./generated_data/orders_data.csv')
 
